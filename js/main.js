6b2b5b82ea6a451b427b1e8ffe662b05facb0daa
Ibukunjoel <ibukunjoelagotola@gmail.com>
2022-08-29 14:47:52 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var i = 0;
+    function add() {
+        document.getElementById('inc').value = ++i;
+    }
+
+var i = 0;
+    function sub() {
+    document.getElementById('inc').value = --i;
+    }
+
+var i = 0;
+    function add1w() {
+        document.getElementById('inc1').value = ++i;
+    }
+
+var i = 0;
+    function sub1w() {
+    document.getElementById('inc1').value = --i;
+    }
+
+var i = 0;
+    function add4() {
+        document.getElementById('inc4').value = ++i;
+    }
+
+var i = 0;
+    function sub4() {
+    document.getElementById('inc4').value = --i;
+    }
